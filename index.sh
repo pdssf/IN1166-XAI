23abcac4714c96780d51b0e027027656c1edf526
Paulo Salgado Filho <paulodiogenes2012@gmail.com>
2025-12-09 16:19:11 -0300

    update script

--- index.sh
+++ index.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#SBATCH --mem 2G
+#SBATCH -c 1
+#SBATCH -p short-complex
+#SBATCH --gpus=1
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=user@cin.ufpe.br
 
+ENV_NAME=$1
+module load Python3.10
+python -m venv $HOME/doc/$ENV_NAME
+source $HOME/doc/$ENV_NAME/bin/activate
+which python
+pip install -r ../requirements.txt
 python3 intrusion_detection_ch.py
