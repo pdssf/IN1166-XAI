a296eed48ff3813c00645530197d8e9f36f79b57
Paulo Salgado Filho <paulodiogenes2012@gmail.com>
2025-12-11 12:00:36 -0300

    feat: atualizando script de execução remota

--- index.sh
+++ index.sh
@@ -4,12 +4,13 @@
 #SBATCH -p short-complex
 #SBATCH --gpus=1
 #SBATCH --mail-type=FAIL,END
-#SBATCH --mail-user=user@cin.ufpe.br
+#SBATCH --mail-user=pdssf@cin.ufpe.br
 
 ENV_NAME=$1
+echo "Environment name: $ENV_NAME"
 module load Python3.10
 python -m venv $HOME/doc/$ENV_NAME
 source $HOME/doc/$ENV_NAME/bin/activate
 which python
-pip install -r ../requirements.txt
+pip install -r ./requirements.txt
 python3 intrusion_detection_ch.py
